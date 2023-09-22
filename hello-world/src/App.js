d75c4dbe59f6d4c5cc9b0559fc9e36f1c0aa0b5a
samadhi-allion <samadhip@alliontechnologies.com>
2023-09-22 14:24:23 +0530

    add test

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Message from './components/Message';
-import Counter from './components/Counter';
+
 
 function App() {
   return (
     <div >
-      <Message/>
-      <Counter/>
+
     </div>
   );
 }
--- hello-world/src/components/Greet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Greet = () => {
-  return (
-    <div>Greet Samadhi</div>
-  )
-}
-
-export default Greet
