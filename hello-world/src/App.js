bc2fc3df0e25c0eaa36101e2b6356c651a8a5d68
samadhi-allion <samadhip@alliontechnologies.com>
2023-09-26 12:48:04 +0530

    add test

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
 
 function App() {
   return (
     <div>
-      <FragmentDemo/>
-      <Table/>
+
     </div>
   );
 }
