9dc3ad28c38dcb912055be2f621415fba13f3da3
samadhi-allion <samadhip@alliontechnologies.com>
2023-09-28 00:09:30 +0530

    add test

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,36 +1,9 @@
 import "./App.css";
-import ClickCounter from "./components/ClickCounter";
-import HoverCounter from "./components/HoverCounter";
-import User from "./components/User";
-import Counter from "./components/Counter";
 
 function App() {
   return (
     <div>
-      <Counter>
-        {(count, incrementCount) => (
-          <ClickCounter count={count} incrementCount={incrementCount} />
-        )}
-      </Counter>
-      <Counter>
-        {(count, incrementCount) => (
-          <HoverCounter count={count} incrementCount={incrementCount} />
-        )}
-      </Counter>
-      {/* <Counter
-        render={(count, incrementCount) => (
-          <ClickCounter count={count} incrementCount={incrementCount} />
-        )}
-      />
-      <Counter
-        render={(count, incrementCount) => (
-          <HoverCounter count={count} incrementCount={incrementCount} />
-        )}
-      /> */}
 
-      {/* <ClickCounter/>
-      <HoverCounter/>
-      <User name={(isLogged)=> isLogged ? 'Samadhi' : 'Gust'} /> */}
     </div>
   );
 }
