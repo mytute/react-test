67dfca58243cb3ba2977075d2a2230ef3fdff746
samadhi-allion <samadhip@alliontechnologies.com>
2023-09-27 14:52:10 +0530

    add test

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,19 +1,9 @@
 import "./App.css";
-import Hero from "./components/Hero";
-import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
     <div>
-      <ErrorBoundary>
-        <Hero heroName="Batman" />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Superman" />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Joker" />
-      </ErrorBoundary>
+
     </div>
   );
 }
