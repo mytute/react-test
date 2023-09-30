119981fabdb2b987a4ad02104aed35153c0773ba
samadhi-allion <samadhip@alliontechnologies.com>
2023-09-30 10:29:44 +0530

    add test

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
-import HookCounter from "./components/HookCounter";
-import User from "./components/User";
-import Items from "./components/Items";
 
 function App() {
   return (
     <div>
-       {/* <HookCounter/> */}
-       {/* <User/> */}
-       <Items/>
+
     </div>
   );
 }
