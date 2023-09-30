03c4fcd529997b6d01fe77cb2791ba76c5f3ee9b
samadhi-allion <samadhip@alliontechnologies.com>
2023-09-30 10:58:48 +0530

    add test

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,13 +1,9 @@
 import "./App.css";
-import ComponentC from "./components/ComponentC";
-import { UserProvider } from "./components/userContext";
 
 function App() {
   return (
     <div>
-      <UserProvider  value="Samadhi">
-        <ComponentC  />
-      </UserProvider>
+
     </div>
   );
 }
