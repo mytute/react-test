5be70ab16fdb384c095ec41fb7e3f0ebfb9c7800
samadhi-allion <samadhip@alliontechnologies.com>
2023-10-01 11:31:18 +0530

    add test

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,18 +1,9 @@
 import "./App.css";
-import HookCounter from "./components/HookCounter";
-import HookMouse from "./components/HookMouse";
-import MouseContainer from "./components/MouseContainer";
-import IntervalHookCounter from "./components/IntervalHookCounter";
-import DataFetching from "./components/DataFetching";
 
 function App() {
   return (
     <div>
-      {/* <HookCounter/>
-      <HookMouse/> */}
-      {/* <MouseContainer/> */}
-      {/* <IntervalHookCounter/> */}
-      <DataFetching/>
+
     </div>
   );
 }
