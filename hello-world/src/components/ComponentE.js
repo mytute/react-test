9bade3f987d215105b6558165cbad45d16a20c6b
samadhi-allion <samadhip@alliontechnologies.com>
2023-10-01 13:16:02 +0530

    add test

--- hello-world/src/components/ComponentE.js
+++ hello-world/src/components/ComponentE.js
@@ -1,15 +1,9 @@
-import React, { useContext } from "react";
+import React from "react";
 import ComponentF from "./ComponentF";
-import { UserContext, ChannelContext } from "../App";
 
 const ComponentE = () => {
-  const user = useContext(UserContext);
-  const channel = useContext(ChannelContext);
   return (
     <div>
-      <div>
-        ComponentE user: {user} | channel: {channel}
-      </div>
       <ComponentF />
     </div>
   );
--- hello-world/src/components/ComponentF.js
+++ hello-world/src/components/ComponentF.js
@@ -1,21 +1,9 @@
 import React from "react";
-import { UserContext, ChannelContext } from "../App";
+
 
 const ComponentF = () => {
   return (
-    <UserContext.Consumer>
-        {
-            (user)=>{
-                return (
-                    <ChannelContext.Consumer>
-                        {(channel)=>{
-                            return <div> user : {user} | channel : {channel}</div>
-                        }}
-                    </ChannelContext.Consumer>
-                )
-            }
-        }
-    </UserContext.Consumer>
+    <div>ComponentF</div>
   );
 };
 
