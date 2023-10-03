4c7613ab2058e53c8e232585e4a5e50e2826e354
samadhi-allion <samadhip@alliontechnologies.com>
2023-10-03 16:20:54 +0530

    add test

--- hello-world/src/components/DateFetching.jsx
+++ hello-world/src/components/DateFetching.jsx
@@ -1,56 +1,30 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const initailState = {
-  post: {},
-  loading: false,
-  error: "",
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "FETCH_SUCCESS":
-      return {
-        post: action.payload,
-        loading: false,
-        error: "",
-      };
-    case "FETCH_ERROR":
-      return {
-        post: {},
-        loading: false,
-        error: "Something went wrong!",
-      };
-    default:
-      return state;
-  }
-};
-
 const DateFetching = () => {
-  const [state, dispatch] = useReducer(reducer, initailState);
+  const [post, setPost] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/posts/1")
       .then((response) => {
-        // setLoading(false);
-        // setPost(response.data);
-        // setError("");
-        dispatch({ type: "FETCH_SUCCESS", payload: response.data });
+        setLoading(false);
+        setPost(response.data);
+        setError("");
       })
       .catch((error) => {
         console.error(error);
-        // setLoading(false);
-        // setPost({});
-        // setError(error.message);
-        dispatch({ type: "FETCH_ERROR", payload: {} });
+        setLoading(false);
+        setPost({});
+        setError(error.message);
       });
   }, []);
-
   return (
     <div>
-      {state.loading ? <p>Loading...</p> : <p></p>}
-      {state.error ? <p>Someting went wrong</p> : <p>{state.post.title}</p>}
+      {loading ? <p>Loading...</p> : <p></p>}
+      {error ? <p>Someting went wrong</p> : <p>{post.title}</p>}
     </div>
   );
 };
