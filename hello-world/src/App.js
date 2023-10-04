45d4f24ddccc86d76b85156d3cb1d358dea7f61e
samadhi-allion <samadhip@alliontechnologies.com>
2023-10-04 16:44:40 +0530

    add test

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
 import FocusInput from "./components/FocusInput";
-import ClassTimer from "./components/ClassTimer";
-import HookTimer from "./components/HookTimer";
-
 
 function App() {
   return (
     <div>
      <FocusInput/>
-     <ClassTimer/>
-     <HookTimer/>
     </div>
   );
 }
